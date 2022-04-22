f04c2c3efd1c3addd1cd55276a7e862cd98f9329
Renata <myska2@yandex.ru>
2022-04-22 19:45:42 +0300

    ex009

---
+++ Ex009_Function/Program.cs
@@ -0,0 +1,20 @@
+﻿// максимальное число из 9 чисел
+int a1 = 10;
+int b1 = 22;
+int c1 = 17;
+int a2 = 4;
+int b2 = 69;
+int c2 = 15;
+int a3 = 5;
+int b3 = 16;
+int c3 = 55;
+int max = a1;
+    if(b1 > max) max = b1;
+    if(c1 > max) max = c1;
+    if(a2 > max) max = a2;
+    if(b2 > max) max = b2;
+    if(c2 > max) max = c2;
+    if(a3 > max) max = a3;
+    if(b3 > max) max = b3;
+    if(c3 > max) max = c3;
+Console.WriteLine(max);
