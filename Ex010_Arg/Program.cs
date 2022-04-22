8491d49fbad34c3433399a29b8f846f9daadf447
Renata <myska2@yandex.ru>
2022-04-22 20:06:28 +0300

    ex010

---
+++ Ex010_Arg/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 10;
+int b1 = 22;
+int c1 = 17;
+int a2 = 4;
+int b2 = 69;
+int c2 = 15;
+int a3 = 5;
+int b3 = 16;
+int c3 = 55;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+Console.WriteLine(max);
